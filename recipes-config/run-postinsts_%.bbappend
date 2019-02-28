d6d512bb19922b92a20d36d81982ed5bcc152e9e
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-02-28 09:31:38 -0800

    Swapfile creation now a one-time thing

--- recipes-config/run-postinsts_%.bbappend
+++ recipes-config/run-postinsts_%.bbappend
@@ -1,9 +1,10 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
-SRC_URI += "file://0_resize-rootfs.sh"
+SRC_URI += "file://0_resize-rootfs.sh file://1_swap-file.sh"
 
 
 do_install_append() {
   install -d ${D}${sysconfdir}/rpm-postinsts
   install -m 0755 ${WORKDIR}/0_resize-rootfs.sh ${D}${sysconfdir}/rpm-postinsts
+  install -m 0755 ${WORKDIR}/1_swap-file.sh ${D}${sysconfdir}/rpm-postinsts
 }
--- recipes-core/minke-run/run.sh
+++ recipes-core/minke-run/run.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-SWAPSIZE=100M
-
 # Install base images. Remove as we go so we don't do it again
 for image in /usr/share/minke/*.tar.gz; do
   if [ "${image}" != "/usr/share/minke/*.tar.gz" ]; then
@@ -10,20 +8,14 @@ for image in /usr/share/minke/*.tar.gz; do
   fi
 done
 
-# Create some swap space
-SWAPFILE=/swapfile
-fallocate --length ${SWAPSIZE} ${SWAPFILE}
-chmod 600 ${SWAPFILE}
-mkswap ${SWAPFILE}
-swapon ${SWAPFILE}
-
 mkdir -p /minke /minke/fs /minke/db /minke/skeletons/local
-touch /etc/timezone 
+touch /etc/timezone /etc/hostname
 
 docker container rm minke
 docker run --name minke \
   -d --privileged \
   -v /etc/timezone:/etc/timezone \
+  -v /etc/hostname:/etc/hostname \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --mount type=bind,source=/minke/fs,target=/minke/fs,bind-propagation=rshared \
   --mount type=bind,source=/minke/db,target=/minke/db,bind-propagation=rshared \
