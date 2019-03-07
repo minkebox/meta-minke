e22601935e15e2241b42563581ef3e8439585ee0
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-07 14:14:10 -0800

    Handle multi line gateways

--- recipes-core/minke-run/prerun.sh
+++ recipes-core/minke-run/prerun.sh
@@ -3,5 +3,5 @@
 # Capture our network address before Docker starts (which might change it)
 IFACE=br0
 IP=$(ip addr show dev ${IFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n1)
-GW=$(ip route show | grep default | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+GW=$(ip route show | grep default | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n1)
 echo "${IP}:${GW}" > /tmp/pre-docker-network
