81ebec34a36833e5a174b1662790d6db2be69dce
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-24 22:46:42 -0700

    Find root disk

--- recipes-core/minke-run/run.sh
+++ recipes-core/minke-run/run.sh
@@ -29,6 +29,9 @@ fi
 rm -rf /etc/resolv.conf
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 
+# Find root disk
+ROOTDISK=$(mount | grep ' / ' | cut -d' ' -f 1 | sed "s:/dev/\(.*\)[0-9]$:\1:")
+
 RESTART_REASON=/tmp/minke-restart-reason
 echo "exit" > ${RESTART_REASON}
 TRACER_OUT=/tmp/tracer.out
@@ -41,6 +44,7 @@ while true; do
   docker run --name minke \
     --privileged \
     --env RESTART_REASON="${REASON}" \
+    --env ROOTDISK="${ROOTDISK}" \
     --mount type=bind,source=/etc/timezone,target=/etc/timezone,bind-propagation=rshared \
     --mount type=bind,source=/etc/hostname,target=/etc/hostname,bind-propagation=rshared \
     --mount type=bind,source=/etc/fstab,target=/etc/fstab,bind-propagation=rshared \
