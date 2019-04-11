00ebf18c92dbc7250fd7ccddde2e7d522c27927c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-04-11 09:27:52 -0700

    Protect these from being re-run

--- recipes-config/run-postinsts/1_swap-file.sh
+++ recipes-config/run-postinsts/1_swap-file.sh
@@ -3,9 +3,10 @@
 SWAPSIZE=512M
 SWAPFILE=/swapfile
 
-fallocate --length ${SWAPSIZE} ${SWAPFILE}
-chmod 600 ${SWAPFILE}
-mkswap ${SWAPFILE}
-swapon ${SWAPFILE}
-
-echo "${SWAPFILE} swap swap defaults 0 0" >> /etc/fstab
+if [ ! -e ${SWAPFILE} ]; then
+  fallocate --length ${SWAPSIZE} ${SWAPFILE}
+  chmod 600 ${SWAPFILE}
+  mkswap ${SWAPFILE}
+  swapon ${SWAPFILE}
+  echo "${SWAPFILE} swap swap defaults 0 0" >> /etc/fstab
+fi
--- recipes-config/run-postinsts/2_fix-boot-disk.sh
+++ recipes-config/run-postinsts/2_fix-boot-disk.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-ROOT=$(mount | grep ' / ' | cut -d' ' -f 1 | sed s/[0-9]$//)
-echo "${ROOT}1 /boot vfat defaults,nofail 0 0" >> /etc/fstab
-mount /boot
+if [ "$(grep -c /boot /etc/fstab)" = "0" ]; then
+  ROOT=$(mount | grep ' / ' | cut -d' ' -f 1 | sed s/[0-9]$//)
+  echo "${ROOT}1 /boot vfat defaults,nofail 0 0" >> /etc/fstab
+  mount /boot
+fi
