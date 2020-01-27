73399d6b2bef0dcfa409e43ecc0c174daf76a0da
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-01-27 15:31:42 -0800

    Force broadcast mode on DHCP relays

--- recipes-core/dhcp-helper/dhcp-helper.c
+++ recipes-core/dhcp-helper/dhcp-helper.c
@@ -16,7 +16,7 @@
 
 /* Author's email: simon@thekelleys.org.uk */
 
-#define COPYRIGHT "Copyright (C) 2004-2012 Simon Kelley" 
+#define COPYRIGHT "Copyright (C) 2004-2012 Simon Kelley"
 
 #include <sys/socket.h>
 #include <sys/ioctl.h>
@@ -37,7 +37,7 @@
 #include <netdb.h>
 #include <linux/types.h>
 #include <linux/capability.h>
-/* There doesn't seem to be a universally-available 
+/* There doesn't seem to be a universally-available
    userpace header for this. */
 extern int capset(cap_user_header_t header, cap_user_data_t data);
 extern int capget(cap_user_header_t header, cap_user_data_t data);
@@ -96,26 +96,26 @@ int main(int argc, char **argv)
   char *runfile = PIDFILE;
   char *user = USER;
   int debug = 0, altports = 0, demonize = 1;
-  
+
   while (1)
     {
       int option = getopt(argc, argv, "b:e:i:s:u:r:dvpn");
-      
+
       if (option == -1)
 	break;
 
-      switch (option) 
+      switch (option)
 	{
 	case 's': case 'b': case 'i': case 'e':
 	  {
 	    struct namelist *new = malloc(sizeof(struct namelist));
-	    
+
 	    if (!new)
 	      {
 		fprintf(stderr, "dhcp-helper: cannot get memory\n");
 		exit(1);
 	      }
-	    
+
 	    strncpy(new->name, optarg, IF_NAMESIZE);
 	    strncpy(ifr.ifr_name, optarg, IF_NAMESIZE);
 	    new->addr.s_addr = 0;
@@ -123,14 +123,14 @@ int main(int argc, char **argv)
 	    if (option == 's')
 	      {
 		struct hostent *e = gethostbyname(optarg);
-		
+
 		if (!e)
 		  {
 		    fprintf(stderr, "dhcp-helper: cannot resolve server name %s\n", optarg);
 		    exit(1);
 		  }
 		new->addr = *((struct in_addr *)e->h_addr);
-	      } 
+	      }
 	    else if (strlen(optarg) > IF_NAMESIZE)
 	      {
 		fprintf(stderr, "dhcp-helper: interface name too long: %s\n", optarg);
@@ -147,7 +147,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "dhcp-helper: interface %s cannot broadcast\n", optarg);
 		exit(1);
 	      }
-	    
+
 	    if (option == 'i')
 	      {
 		new->next = interfaces;
@@ -165,7 +165,7 @@ int main(int argc, char **argv)
 	      }
 	  }
 	  break;
-	  
+
 	case 'u':
 	  if ((user = malloc(strlen(optarg) + 1)))
 	    strcpy(user, optarg);
@@ -175,11 +175,11 @@ int main(int argc, char **argv)
 	  if ((runfile = malloc(strlen(optarg) + 1)))
 	    strcpy(runfile, optarg);
 	  break;
-	  
+
 	case 'd':
 	  debug = 1;
 	  break;
-	  
+
 	case 'p':
 	  altports = 1;
 	  break;
@@ -191,9 +191,9 @@ int main(int argc, char **argv)
 	case 'v':
 	  fprintf(stderr, "dhcp-helper version %s, %s\n", VERSION, COPYRIGHT);
 	  exit(0);
-	  
+
 	default:
-	  fprintf(stderr, 
+	  fprintf(stderr,
 		  "Usage: dhcp-helper [OPTIONS]\n"
 		  "Options are:\n"
 		  "-s <server>      Forward DHCP requests to <server>\n"
@@ -208,14 +208,14 @@ int main(int argc, char **argv)
 		  "-v               Give version and copyright info and then exit\n",
 		  USER, PIDFILE);
 	  exit(1);
-	  
+
 	}
     }
 
   if (!servers)
     {
       fprintf(stderr, "dhcp-helper: no destination specifed; give at least -s or -b option.\n");
-      exit(1); 
+      exit(1);
     }
 
   if (!(packet = malloc(buf_size)))
@@ -223,21 +223,21 @@ int main(int argc, char **argv)
       perror("dhcp-helper: cannot allocate buffer");
       exit(1);
     }
-  
+
   if (fd == -1 && (fd = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
     {
       perror("dhcp-helper: cannot create socket");
       exit(1);
     }
-  
+
   if (setsockopt(fd, SOL_IP, IP_PKTINFO, &oneopt, sizeof(oneopt)) == -1 ||
       setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &oneopt, sizeof(oneopt)) == -1 ||
-      setsockopt(fd, SOL_IP, IP_MTU_DISCOVER, &mtuopt, sizeof(mtuopt)) == -1)  
+      setsockopt(fd, SOL_IP, IP_MTU_DISCOVER, &mtuopt, sizeof(mtuopt)) == -1)
     {
       perror("dhcp-helper: cannot set options on DHCP socket");
       exit(1);
     }
-  
+
   saddr.sin_family = AF_INET;
   saddr.sin_port = htons(altports ? DHCP_SERVER_ALTPORT : DHCP_SERVER_PORT);
   saddr.sin_addr.s_addr = INADDR_ANY;
@@ -256,17 +256,17 @@ int main(int argc, char **argv)
       struct group *gp;
       cap_user_header_t hdr = malloc(sizeof(*hdr));
       cap_user_data_t data = NULL;
-      
+
       if (getuid() == 0)
 	{
 	  if (hdr)
 	    {
 	      int capsize = 1;
-	      
+
 	      /* find version supported by kernel */
 	      memset(hdr, 0, sizeof(*hdr));
 	      capget(hdr, NULL);
-	      
+
 	      if (hdr->version != LINUX_CAPABILITY_VERSION_1)
 		{
 		  /* if unknown version, use largest supported version (3) */
@@ -274,85 +274,85 @@ int main(int argc, char **argv)
 		    hdr->version = LINUX_CAPABILITY_VERSION_3;
 		  capsize = 2;
 		}
-	      
+
 	      if ((data = malloc(sizeof(*data) * capsize)))
 		memset(data, 0, sizeof(*data) * capsize);
 	    }
-	  
+
 	  if (!hdr || !data)
 	    {
 	      perror("dhcp-helper: cannot allocate memory");
 	      exit(1);
 	    }
-	  
+
 	  hdr->pid = 0; /* this process */
 	  data->effective = data->permitted = data->inheritable =
 	    (1 << CAP_NET_ADMIN) | (1 << CAP_SETGID) | (1 << CAP_SETUID);
-	  
+
 	  /* Tell kernel to not clear capabilities when dropping root */
 	  if (capset(hdr, data) == -1 || prctl(PR_SET_KEEPCAPS, 1) == -1)
 	    {
 	      perror("dhcp-helper: cannot set kernel capabilities");
 	      exit(1);
 	    }
-	  
+
 	  if (!ent_pw)
 	    {
 	      fprintf(stderr, "dhcp-helper: cannot find user %s\n", user);
 	      exit(1);
 	    };
-	}	  
-      
+	}
+
       if (chdir("/") == -1)
 	{
 	  perror("dhcp-helper: cannot change directory");
 	   exit(1);
 	}
-      
+
       if (demonize) {
 	/* The following code "daemonizes" the process.
 	   See Stevens section 12.4 */
-	
+
 	if (fork() != 0 )
 	  _exit(0);
-	
+
 	setsid();
-	
+
 	if (fork() != 0)
 	  _exit(0);
       }
-      
+
       umask(022); /* make pidfile 0644 */
-      
+
       /* write pidfile _after_ forking ! */
       if ((pidfile = fopen(runfile, "w")))
 	{
 	  fprintf(pidfile, "%d\n", (int) getpid());
 	  fclose(pidfile);
         }
-      
+
       umask(0);
-      
-      for (i=0; i<64; i++)        
+
+      for (i=0; i<64; i++)
 	if (i != fd)
 	  close(i);
-      
+
       if (getuid() == 0)
 	{
 	  setgroups(0, &dummy);
-	  
+
 	  if ((gp = getgrgid(ent_pw->pw_gid)))
 	    i = setgid(gp->gr_gid);
-	  i = setuid(ent_pw->pw_uid); 
-	  
+	  i = setuid(ent_pw->pw_uid);
+
 	  data->effective = data->permitted = 1 << CAP_NET_ADMIN;
 	  data->inheritable = 0;
-	  
+
 	  /* lose the setuid and setgid capbilities */
-	  capset(hdr, data);	
+	  capset(hdr, data);
 	}
     }
-  
+
   while (1) {
     int iface_index;
     struct interface *iface;
@@ -365,7 +365,7 @@ int main(int argc, char **argv)
       struct cmsghdr align; /* this ensures alignment */
       char control[CMSG_SPACE(sizeof(struct in_pktinfo))];
     } control_u;
-        
+
     msg.msg_control = control_u.control;
     msg.msg_controllen = sizeof(control_u);
     msg.msg_name = &saddr;
@@ -374,29 +374,29 @@ int main(int argc, char **argv)
     msg.msg_iovlen = 1;
     iov.iov_base = packet;
     iov.iov_len = buf_size;
-    
+
     while (1) {
       struct dhcp_packet *newbuf;
       size_t newsz;
 
       msg.msg_flags = 0;
       while((sz = recvmsg(fd, &msg, MSG_PEEK)) == -1 && errno == EINTR);
-      
+
       if (sz == -1 || !(msg.msg_flags & MSG_TRUNC) ||
 	  !(newbuf = realloc(packet, (newsz = buf_size + 100))))
 	break;
-      
+
       iov.iov_base = packet = newbuf;
       iov.iov_len = buf_size = newsz;
     }
-    
+
     while ((sz = recvmsg(fd, &msg, 0)) == -1 && errno == EINTR);
-    
+
     if ((msg.msg_flags & MSG_TRUNC) ||
-	sz < (ssize_t)(sizeof(struct dhcp_packet)) || 
+	sz < (ssize_t)(sizeof(struct dhcp_packet)) ||
 	msg.msg_controllen < sizeof(struct cmsghdr))
       continue;
-    
+
     iface_index = 0;
     for (cmptr = CMSG_FIRSTHDR(&msg); cmptr; cmptr = CMSG_NXTHDR(&msg, cmptr))
       if (cmptr->cmsg_level == SOL_IP && cmptr->cmsg_type == IP_PKTINFO)
@@ -408,10 +408,10 @@ int main(int argc, char **argv)
           p.c = CMSG_DATA(cmptr);
           iface_index = p.p->ipi_ifindex;
 	}
-    
+
     if (!(ifr.ifr_ifindex = iface_index) || ioctl(fd, SIOCGIFNAME, &ifr) == -1)
       continue;
-    	 
+
     /* last ditch loop squashing. */
     if ((packet->hops++) > 20)
       continue;
@@ -423,7 +423,7 @@ int main(int argc, char **argv)
       {
 	/* message from client */
 	struct namelist *tmp;
-	
+
 	/* packets from networks we are broadcasting _too_
 	   are explicitly not allowed to be forwarded _from_ */
 	for (tmp = servers; tmp; tmp = tmp->next)
@@ -438,16 +438,16 @@ int main(int argc, char **argv)
 	    break;
 	if (tmp)
 	  continue;
-	
+
 	if (interfaces)
-	  { 
+	  {
 	    for (tmp = interfaces; tmp; tmp = tmp->next)
 	      if (strncmp(tmp->name, ifr.ifr_name, IF_NAMESIZE) == 0)
 		break;
 	    if (!tmp)
 	      continue;
 	  }
-	
+
 	/* already gatewayed ? */
 	if (packet->giaddr.s_addr)
 	  {
@@ -465,7 +465,7 @@ int main(int argc, char **argv)
 	    ifr.ifr_addr.sa_family = AF_INET;
 	    if (ioctl(fd, SIOCGIFADDR, &ifr) == -1)
 	      continue;
-	    
+
 	    iface_addr = packet->giaddr = ((struct sockaddr_in *) &ifr.ifr_addr)->sin_addr;
 	    /* build address->interface index table for returning answers */
 	    for (iface = ifaces; iface; iface = iface->next)
@@ -474,7 +474,7 @@ int main(int argc, char **argv)
 		  iface->index = iface_index;
 		  break;
 		}
-	    
+
 	    /* not there, add a new entry */
 	    if (!iface && (iface = malloc(sizeof(struct interface))))
 	      {
@@ -484,7 +484,7 @@ int main(int argc, char **argv)
 		iface->index = iface_index;
 	      }
 	  }
-	
+
 	/* send to all configured servers. */
 	for (tmp = servers; tmp; tmp = tmp->next)
 	  {
@@ -498,29 +498,31 @@ int main(int argc, char **argv)
 	      }
 	    else
 	      saddr.sin_addr = tmp->addr;
-	    
+
 	    saddr.sin_port = htons(altports ? DHCP_SERVER_ALTPORT : DHCP_SERVER_PORT);
+            // Force all replies to be broadcast back
+            packet->flags |= htons(0x8000);
 	    while(sendto(fd, packet, sz, 0, (struct sockaddr *)&saddr, sizeof(saddr)) == -1 &&
 		  errno == EINTR);
 	  }
       }
     else if (packet->op == BOOTREPLY)
-      { 
-	/* packet from server send back to client */	
+      {
+	/* packet from server send back to client */
 	saddr.sin_port = htons(altports ? DHCP_CLIENT_ALTPORT : DHCP_CLIENT_PORT);
 	msg.msg_control = NULL;
 	msg.msg_controllen = 0;
 	msg.msg_namelen = sizeof(saddr);
 	iov.iov_len = sz;
-			   
+
 	/* look up interface index in cache */
 	for (iface = ifaces; iface; iface = iface->next)
 	  if (iface->addr.s_addr == packet->giaddr.s_addr)
 	    break;
-	
+
 	if (!iface)
 	  continue;
-            
+
 	if (packet->ciaddr.s_addr)
 	  saddr.sin_addr = packet->ciaddr;
 	else if (ntohs(packet->flags) & 0x8000 || !packet->yiaddr.s_addr || packet->hlen > 14)
@@ -537,9 +539,9 @@ int main(int argc, char **argv)
 	    cmptr->cmsg_level = SOL_IP;
 	    cmptr->cmsg_type = IP_PKTINFO;
 	  }
-	else 
+	else
 	  {
-	    /* client not configured and cannot reply to ARP. 
+	    /* client not configured and cannot reply to ARP.
 	       Insert arp entry direct.*/
 	    saddr.sin_addr = packet->yiaddr;
 	    ifr.ifr_ifindex = iface->index;
@@ -560,5 +562,5 @@ int main(int argc, char **argv)
       }
   }
 }
-    
-	    
+
+
