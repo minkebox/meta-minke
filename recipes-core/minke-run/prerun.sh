2fca1279ca5add4ed1bdc1b3cf0f838a36e49c47
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-01-27 15:31:13 -0800

    Improved default ip setup

--- recipes-core/minke-run/prerun.sh
+++ recipes-core/minke-run/prerun.sh
@@ -11,14 +11,24 @@ else
 fi
 GW=$(ip route show | grep default | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n1)
 
-# If network failed to start, we assign a default address
-if [ "${IP}" = "" ]; then
-  IP=192.168.1.200
-  GW=192.168.1.1
-  BCAST=192.168.1.255
-  ip addr add ${IP}/24 broadcast ${BCAST} dev ${IFACE}
-  ip link set ${IFACE} up
-  ip route add default via ${GW}
+# Note first run
+FIRSTRUN=$(test -f /usr/share/minke/prerun-done || echo 'yes')
+touch /usr/share/minke/prerun-done
+
+# If network failed to start the first time, we assign a default address and reboot
+if [ "${FIRSTRUN}" = "yes" -a "${IP}" = "" -a "${IFACE}" = "br0" ]; then
+  cat > /lib/systemd/network/70-bridge.network <<__EOF__
+[Match]
+Name=br0
+
+[Network]
+Address=192.168.1.200/24
+Gateway=192.168.1.1
+
+[DHCP]
+UseDNS=false
+__EOF__
+  reboot
 fi
 
 echo "${IP}:${GW}" > /tmp/pre-docker-network
