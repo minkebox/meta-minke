7af732187680d8ca6347e971f002cc2a4a361fde
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-02-18 17:07:44 -0800

    More logging

--- recipes-core/minke-run/run.sh
+++ recipes-core/minke-run/run.sh
@@ -96,7 +96,7 @@ while true; do
     --mount type=bind,source=/minke/skeletons/internal,target=/app/skeletons/internal,bind-propagation=rshared \
     --mount type=bind,source=/mnt,target=/mnt,bind-propagation=rshared \
     --network=host \
-    --log-driver json-file --log-opt max-size=10k --log-opt max-file=1 \
+    --log-driver json-file --log-opt max-size=100k --log-opt max-file=1 \
     registry.minkebox.net/minkebox/minke
   case "$(cat ${RESTART_REASON})" in
     update-native) cp /dev/null ${TRACER_OUT} ; systemctl start dnf-automatic-restart ;;
